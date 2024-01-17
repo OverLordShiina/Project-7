3510c993b2e0761e2904edc4109df73c98e4ecad
Lucas Dacalor <lucas.dacalor@outlook.fr>
2024-01-17 16:10:22 -0400

    banner fix

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,13 @@
 import "../style/Banner.css";
+import { useLocation } from "react-router-dom";
 
 function Banner({ bannerBg, text = "" }) {
+  const location = useLocation();
+  const pathName = location.pathname;
+  const isAbout = "banner-pic about-banner";
+
   return (
-    <div className="acl-banner">
+    <div className={pathName === "/about" ? isAbout : "banner-pic"}>
       <img src={bannerBg} alt="banner background" />
       <h1 className="banner-text">{text}</h1>
     </div>
